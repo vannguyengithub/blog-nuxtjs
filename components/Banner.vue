6807ddb249c07780b9409dc25215bdffd6e89a9c
van nguyen <vannguyengithub@gmail.com>
2023-07-13 17:27:51 +0700

    up

--- components/Banner.vue
+++ components/Banner.vue
@@ -5,12 +5,12 @@
 
         <div class="swiper-slide">
           <nuxt-link to="#">
-            <img src="https://img.freepik.com/free-vector/paper-style-chinese-new-year-festival-celebration-horizontal-sale-banner-template_23-2149923337.jpg?w=996&t=st=1688881866~exp=1688882466~hmac=149e640931de17e0d8c34733b6b6a7e2aa2ae1829e9cc34c4ee38152b1015e10" alt="slider">
+            <img src="/assets/images/banner1.jpg" alt="slider">
           </nuxt-link>
         </div>
         <div class="swiper-slide">
           <nuxt-link to="#">
-            <img src="https://img.freepik.com/free-vector/paper-style-chinese-new-year-festival-celebration-horizontal-sale-banner-template_23-2149923337.jpg?w=996&t=st=1688881866~exp=1688882466~hmac=149e640931de17e0d8c34733b6b6a7e2aa2ae1829e9cc34c4ee38152b1015e10" alt="slider">
+            <img src="/assets/images/banner1.jpg" alt="slider">
           </nuxt-link>
         </div>
 
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,36 @@
 <template>
   <main>
+    <div class="container">
 
+      <div class="social">
+        <a href="https://twitter.com/steve_walson" target="_blank" class="twitter">
+          <svg viewBox="126.444 2.281 589 589" height="42">
+            <title>Twitter</title>
+            <path d="M609.773 179.634c-13.891 6.164-28.811 10.331-44.498 12.204 16.01-9.587 28.275-24.779 34.066-42.86a154.78 154.78 0 0 1-49.209 18.801c-14.125-15.056-34.267-24.456-56.551-24.456-42.773 0-77.462 34.675-77.462 77.473 0 6.064.683 11.98 1.996 17.66-64.389-3.236-121.474-34.079-159.684-80.945-6.672 11.446-10.491 24.754-10.491 38.953 0 26.875 13.679 50.587 34.464 64.477a77.122 77.122 0 0 1-35.097-9.686v.979c0 37.54 26.701 68.842 62.145 75.961-6.511 1.784-13.344 2.716-20.413 2.716-4.998 0-9.847-.473-14.584-1.364 9.859 30.769 38.471 53.166 72.363 53.799-26.515 20.785-59.925 33.175-96.212 33.175-6.25 0-12.427-.373-18.491-1.104 34.291 21.988 75.006 34.824 118.759 34.824 142.496 0 220.428-118.052 220.428-220.428 0-3.361-.074-6.697-.236-10.021a157.855 157.855 0 0 0 38.707-40.158z" />
+          </svg>
+        </a>
+
+<a href="https://www.facebook.com/steve.wals0n" target="_blank" class="facebook">
+  <svg viewBox="0 0 1700 1700" height="27">
+    <title>Facebook</title>
+    <path d="M1376 128q119 0 203.5 84.5t84.5 203.5v960q0 119-84.5 203.5t-203.5 84.5h-188v-595h199l30-232h-229v-148q0-56 23.5-84t91.5-28l122-1v-207q-63-9-178-9-136 0-217.5 80t-81.5 226v171h-200v232h200v595h-532q-119 0-203.5-84.5t-84.5-203.5v-960q0-119 84.5-203.5t203.5-84.5h960z"></path>
+  </svg>
+</a>
+        <a href="https://instagram.com/steve_walson" target="_blank" class="instagram">
+          <svg viewBox="0 0 24 24" height="32">
+            <title>Instagram</title>
+            <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z" />
+          </svg>
+        </a>
+        <a href="#" class="linkdin">
+          <svg role="img" viewBox="0 0 24 24" height="23">
+            <title>LinkedIn</title>
+            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+          </svg>
+        </a>
+      </div>
+      <div class="profile"></div>
+    </div>
   </main>
 </template>
 
@@ -11,5 +41,132 @@ export default {
 </script>
 
 <style scoped>
+main {
+  min-height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+:root {
+  --prim: transparent;
+  --sec: #fcdc6a21;
+}
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+  height: 400px;
+  width: 400px;
+  position: relative;
+  margin: 0 auto;
+}
+
+.profile {
+  background-image: url(../static/assets/images/meo-meo.jpg);
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  right: 50%;
+  top: 50%;
+  transform: translate(50%, -50%);
+  border-radius: 50%;
+  transition: transform 0.4s ease-in-out;
+}
+
+.profile::before {
+  content: "Contact Me";
+  font-family: poppins;
+  font-weight: bold;
+  color: #fff;
+  padding: 5px 8px;
+  position: absolute;
+  bottom: -4%;
+  right: 50%;
+  transform: translateX(50%);
+  border-radius: 9999px;
+  background-color: #008da0;
+}
+
+.social {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  place-items: center;
+  gap: 150px;
+  transform: scale(0.5);
+  transform-origin: center;
+  transform-box: fill-box;
+}
+
+.social a {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  place-items: center;
+  display: grid;
+  fill: #fff;
+  transition: all 0.1s ease-in-out;
+  position: relative;
+}
+.social a:hover,
+.social a:focus {
+  background-color: #aab8c2;
+}
+.social a:hover > svg,
+.social a:focus > svg {
+  transform: scale(1.2);
+  transition: all 0.2s ease-in-out;
+}
+
+.social .twitter {
+  background-color: #1da1f2;
+}
+.social .facebook {
+  background-color: #4267b2;
+}
+
+.social .linkdin {
+  background-color: #0072b1;
+  clip-path: circle(50% at 50% 50%);
+}
+.social .instagram {
+  background-color: #e1306c;
+}
+
+.container:hover .social {
+  transform: scale(1);
+  animation: rotate 20s linear infinite;
+}
+
+.container:hover .social:hover {
+  animation-play-state: paused;
+}
+.container:hover .profile {
+  transform: translate(50%, -50%) scale(0.8);
+}
+
+@keyframes rotate {
+  100% {
+    transform: scale(1) rotate(360deg);
+  }
+}
 
 </style>
