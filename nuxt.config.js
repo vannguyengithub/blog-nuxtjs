3941db3f5476ed1d7c3fe1e45c70b9212d284cb6
Macbook Pro <>
2024-03-03 17:40:13 +0700

    fix link api

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ export default {
 
   },
   env: {
-    baseUrl: process.env.BASE_URL || 'https://api.thosuabep.com/wp-json',
+    baseUrl: process.env.BASE_URL || 'http://api.thosuabep.com/wp-json',
     // baseUrl: process.env.BASE_URL || 'http://learning-nuxtjs.vn/wp-api/wp-json',
     // http://learning-nuxtjs.vn/wp-api/wp-json/wp/v2
   }
